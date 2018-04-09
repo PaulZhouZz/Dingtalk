26c9d7acfe2f2d35782ff4c4e3bd705d735db18b
Paul Zhou <paul.zhou@trustasia.com>
2018-04-09 14:34:59 +0800

    改为文件缓存accesstoken和jsticket

--- src/util/Cache.php
+++ src/util/Cache.php
@@ -1,55 +1,33 @@
 <?php
 namespace Dingtalk\util;
-use DB;
 class Cache {
 
     public function setJsTicket($ticket)
     {
-        $db = DB::connection('mongodb')->collection('mymongo')->where('name','js_ticket')->get();
-        if (empty($db))
-            DB::connection('mongodb')->collection('mymongo')->update(['ticket'=>$ticket,'expires'=>time()+7000]);
-        else
-            DB::connection('mongodb')->collection('mymongo')->insert(['name'=>'js_ticket','ticket'=>$ticket,'expires'=>time()+7000]);
-//        $memcache = $this->getMemcache();
-//        $memcache->set("js_ticket", $ticket, time() + 7000); // js ticket有效期为7200秒，这里设置为7000秒
+        $memcache = $this->getMemcache();
+        $memcache->set("js_ticket", $ticket, time() + 7000); // js ticket有效期为7200秒，这里设置为7000秒
     }
 
     public function getJsTicket()
     {
-        $res = DB::connection('mongodb')->collection('mymongo')->where('name','js_ticket')->get();
-        foreach ($res as $rs ) if ($rs['expires'] > time()) return $rs['ticket'];
-
-        return false;
-
-//        $memcache = $this->getMemcache();
-//        return $memcache->get("js_ticket");
+        $memcache = $this->getMemcache();
+        return $memcache->get("js_ticket");
     }
 
     public function setCorpAccessToken($accessToken)
     {
-        $db = DB::connection('mongodb')->collection('mymongo')->where('name','corp_access_token')->get();
-        if (empty($db))
-            DB::connection('mongodb')->collection('mymongo')->update(['accesstoken' => $accessToken,'expires' => time()+7000]);
-        else
-            DB::connection('mongodb')->collection('mymongo')->insert(['name' => 'corp_access_token','accesstoken' => $accessToken,'expires' => time()+7000]);
-//        $memcache = $this->getMemcache('corp_access_token',$accessToken, time() + 7000);
-//        $memcache->set("corp_access_token", $accessToken, time() + 7000); // corp access token有效期为7200秒，这里设置为7000秒
+        $memcache = $this->getMemcache();
+        $memcache->set("corp_access_token", $accessToken, time() + 7000); // corp access token有效期为7200秒，这里设置为7000秒
     }
 
     public function getCorpAccessToken($name)
     {
-        $res = DB::connection('mongodb')->collection('mymongo')->where('name', $name)->get();
-        foreach ($res as $rs ) if ($rs['expires'] > time()) return $rs['accesstoken'];
-
-        return false;
-
-//        $memcache = $this->getMemcache();
-//        return $memcache->get("corp_access_token");
+        $memcache = $this->getMemcache();
+        return $memcache->get("corp_access_token");
     }
 
-    private function getMemcache($name,$accessToken,$expires)
+    private function getMemcache()
     {
-        DB::connection('mongodb')->collection('mymongo')->insert(['name' => $name,'accesstoken' => $accessToken,'expires' => $expires]);
         return new FileCache;
     }
 
